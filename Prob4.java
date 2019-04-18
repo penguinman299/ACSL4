51eedc15bf65c8b2ac2952274906b1d535126c63
Stephan <penguinman299@gmail.com>
2019-04-18 16:29:45 -0400

    Filled in main method

--- Prob4.java
+++ Prob4.java
@@ -1,5 +1,23 @@
+import java.utils.*;
+
 public class Prob4{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        int n = scam.nextInt();
+        for(int i = 0; i < n; i++){
+            String prefix = scan.nextLine();
+            ArrayList<String> equation = new ArrayList<>;
+            for(int t = 0; t<prefix.length; t++){
+                equation.add(prefix.charAt(t));
+            }
+            while(equation.size()!=1){
+                equation = solver(equation);
+            }
+            System.out.println(equation.get(0));
+        }
+    }
+
+    public static ArrayList<String> solver(new ArrayList<String> prefix){
+        retutn new ArrayList<String>;
     }
 }
