4e8bb96d2b918460cce2e57061a1cb93fe2b3374
Braeden Mathers <mathersb22@hsnet.ahsd.org>
2019-04-23 15:29:19 -0400

    added prefix operator code

--- Prob4.java
+++ Prob4.java
@@ -2,4 +2,22 @@ public class Prob4{
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static int sub(int a, int b){
+        return a - b;
+    }
+    public static int mult(int a, int b){
+        return a * b;
+    }
+    public static int switcher(int a, int b, int c){ // that's the weird @ thing
+        if (a >= 0){
+            return b;
+        }
+        return c;
+    }
+    public static int max(int a, int b, int c){
+        return Math.max(a,Math.max(b,c));
+    }
 }
